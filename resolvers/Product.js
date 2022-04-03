9fc678c88e47671f4f9ca0f488c78a31054fcf9e
Talal <itxtalal@gmail.com>
2022-04-03 23:18:06 +0500

    Adding Reviews Object Type

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -8,4 +8,8 @@ exports.Product = {
     // const categoryId = parent.categoryId;
     return categories.find((category) => category.id === categoryId);
   },
+
+  reviews: ({ id }, args, { reviews }) => {
+    return reviews.filter((review) => review.productId === id);
+  },
 };
--- schema.js
+++ schema.js
@@ -19,11 +19,21 @@ exports.typeDefs = gql`
     image: String!
     onSale: Boolean!
     category: Category!
+    reviews: [Review]!
   }
 
   type Category {
     id: ID!
     name: String!
     products: [Product!]!
+  }
+
+  type Review {
+    id: ID!
+    date: String!
+    title: String!
+    comment: String!
+    rating: Int!
+    productId: ID!
   }
 `;
